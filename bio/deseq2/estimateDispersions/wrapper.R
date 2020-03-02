0d1266dd3da6c12cff2c7cc97542e99f66dbf737
tdayris <thibault.dayris@outlook.fr>
2020-03-02 17:10:51 +0100

    [dev] (Options): Optional parameters are now really optional

--- bio/deseq2/estimateDispersions/wrapper.R
+++ bio/deseq2/estimateDispersions/wrapper.R
@@ -4,18 +4,28 @@
 base::library("DESeq2");     # Differential Gene expression
 
 # Cast input path as character
-dds_path <- base::as.character(snakemake@input[["dds"]]);
+dds_path <- base::as.character(x = snakemake@input[["dds"]]);
 dds <- base::readRDS(dds_path);
 
 # Cast locfunc as function name
-fittype <- base::as.character(snakemake@params[["fittype"]]);
-
+extra <- "";
+if ("extra" %in% names(snakemake@params)) {
+  extra <- base::paste0(
+    ", ",
+    base::as.character(x = snakemake@params[["extra"]])
+  );
+}
 # Create object
-dds <- DESeq2::estimateDispersions(dds, fitType = fittype);
-print(dds);
+dds <- base::eval(
+  base::parse(
+    text = base::paste0(
+      "DESeq2::estimateDispersions(dds", extra, ");"
+    )
+  )
+);
 
 # Save as RDS
-output_path <- base::as.character(snakemake@output[["disp"]]);
+output_path <- base::as.character(x = snakemake@output[["disp"]]);
 base::saveRDS(
   obj = dds,
   file = output_path
