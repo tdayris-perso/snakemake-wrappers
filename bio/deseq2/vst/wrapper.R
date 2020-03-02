03135948e43ea352a363a357fd12f836f84e495b
tdayris <thibault.dayris@outlook.fr>
2020-03-02 15:08:57 +0100

    [dev] (Initial commit): Complete untested wrapper

---
+++ bio/deseq2/vst/wrapper.R
@@ -0,0 +1,39 @@
+# This script takes a deseq2 dataset object and performs
+# a variance stabilizing transformations transformation on it
+
+base::library("DESeq2");     # Differential Gene expression
+
+# Cast input path as character
+dds_path <- base::as.character(x = snakemake@input[["dds"]]);
+dds <- base::readRDS(file = dds_path);
+
+# Recover extra parameters
+extra <- base::as.character(
+  x = snakemake@params[["extra"]]
+)
+
+# Create object
+vst <- base::eval(
+  base::pasrse(
+    text = base::paste0(
+      "DESeq2::vst(object = dds, ", extra, ");"
+    )
+  )
+);
+
+# Save results
+output_rds <- base::as.character(snakemake@output[["rds"]]);
+base::saveRDS(
+  obj = vst,
+  file = output_rds
+);
+
+
+output_table <- base::as.character(snakemake@output[["tsv"]]);
+tsv <- SummarizedExperiment::assay(vst);
+utils::write.csv(
+  x = tsv,
+  file = output_table,
+  sep = "\t",
+  quote = FALSE
+);
