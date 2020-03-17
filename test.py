9b57bad4565d57c5bcf671e90919d36b1c3c2f7d
tdayris-perso <tdayris@tutanota.de>
2020-03-17 14:31:55 +0100

    [dev] (Tests): Fixed all tests

--- test.py
+++ test.py
@@ -1163,12 +1163,12 @@ def test_bwa_mem_samblaster():
 
 
 def test_snpsift_vartype():
-    run("bio/snpsift/varType", ["snakemake", "out.vcf", "--use-conda", "-F"])
+    run("bio/snpsift/varType", ["snakemake", "annotated/out.vcf", "--use-conda", "-F"])
 
 
 def test_snpsift_annotate():
-    run("bio/snpsift/annotate", ["snakemake", "out.vcf", "--use-conda", "-F"])
+    run("bio/snpsift/annotate", ["snakemake", "annotated/out.vcf", "--use-conda", "-F"])
 
 
 def test_snpsift_gwascat():
-    run("bio/snpsift/gwasCat", ["snakemake", "out.vcf", "--use-conda", "-F"])
+    run("bio/snpsift/gwasCat", ["snakemake", "annotated/out.vcf", "--use-conda", "-F"])
